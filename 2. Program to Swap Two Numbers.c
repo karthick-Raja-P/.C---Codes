5b22718bbc1d6aa5327b49ba8bcbc1c513a1fe40
Karthick_Raja <110902640+karthick-Raja-P@users.noreply.github.com>
2022-08-27 18:35:15 +0530

    Add files via upload

---
+++ 2. Program to Swap Two Numbers.c
@@ -0,0 +1,22 @@
+// Program to Swap Two Numbers //
+
+#include <stdio.h>
+
+ void main () 
+{
+  
+int A = 5, B = 4;
+  
+A = A + B;
+  
+B = A - B;
+  
+A = A - B;
+  
+printf ("A: %d \nB: %d", A, B);
+
+}
+
+/* Output: 
+A: 4 
+B: 5 /*
